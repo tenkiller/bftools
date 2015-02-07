42c65baa9a33b86a5d66a7d077258e73e228f31d
tenkiller <fincher.brett@gmail.com>
2015-02-07 10:46:17 -0600

    Updated nav widget

--- client/app/widgets/nav/nav.controller.js
+++ client/app/widgets/nav/nav.controller.js
@@ -1,12 +1,36 @@
 (function () {
   'use strict';
-  angular.module('bfTools').controller('navigationController', navigationController);
+  angular.module('pacgc').controller('navigationController', navigationController);
   
-  navigationController.$inject = ['$scope'];
+  navigationController.$inject = ['$scope', '$location'];
 
-  function navigationController($scope) {
-
+  function navigationController($scope, $location) {
+    var vm = this;
+    
+    vm.nav = [];
+    vm.isActive = isActive;
+    
+    init();
+    
     //#region internal functions
+    
+    function init() {
+      var config = $scope.config;
+      
+      if (angular.isObject(config)) {
+        // TODO: validate the config object before using it
+        vm.nav = config;
+      } else if (angular.isString(config)) {
+        // TODO: validate scope.config against a URL regex before using $http
+        $http.get(config).success(function (data) {
+          vm.nav = data.nav;
+        });
+      }
+    }
+    
+    function isActive(path) {
+      return path === $location.path();
+    }
     
     //#endregion
   }
